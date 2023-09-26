c9cd5fa1811f40319e8c11373eb34399da4cf7c6
davenmir <davenmir95@gmail.com>
2023-09-26 18:49:44 -0500

    teehee

--- components/NavComponent.js
+++ components/NavComponent.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import styles from "../styles/NavComponent.module.css";
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 const NavComponent = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
+
 
   const handleClick = () => {
     setModalOpen(!modalOpen);
@@ -16,11 +19,11 @@ const NavComponent = () => {
       </button>
       {modalOpen && (
         <div className="modal">
-          <ul className="nav-links">
-            <Link href="/">
+          <ul className={styles.navlinks}>
+            <Link href="/" onClick={() => router.push("/")}>
               <p className={styles.link}>Home</p>
             </Link>
-            <Link href="/ToDo">
+            <Link href="/ToDo" onClick={() => router.push("/ToDo")}>
               <p className={styles.link}>To Do</p>
             </Link>
           </ul>
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,9 @@
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
+
+  
+
   return (
     <>
       <div className={styles.pageBody}>
